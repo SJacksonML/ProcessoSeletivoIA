01b70baeb38d3786d1a0eaac4cd9fd985dfc1045
Jackson <samuel.jackson@aluno.ufca.edu.br>
2026-04-26 02:05:59 -0300

    Modelo otimizado
    Implementação inicial do Modelo otimizado

--- optimize_model.py
+++ optimize_model.py
@@ -1,4 +1,21 @@
 import tensorflow as tf
 import os
 
-#insira seu código aqui
+# Carregando o modelo treinado:
+
+model = tf.keras.models.load_model('model.h5')
+
+# Convertendo o modelo para o formato .tflite:
+
+converter = tf.lite.TFLiteConverter.from_keras_model(model) # Dynamic Range Quantization
+
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+
+tflite_model = converter.convert()
+
+# Salvando o modelo agora otimizado no arquivo 'model.tflite'
+
+with open('model.tflite', 'wb') as f:
+    f.write(tflite_model)
+
+print("Modelo otimizado.")
